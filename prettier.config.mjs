03248b15c0a205ac0252c9828f4b9bb768a726fb
Nicholas Lim <nicknitewolf@gmail.com>
2025-04-01 23:21:42 +0800

    Set-up prettier for my own sanity

---
+++ prettier.config.mjs
@@ -0,0 +1,21 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  endOfLine: "lf",
+  embeddedLanguageFormatting: "auto",
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+};
+
+export default config;
